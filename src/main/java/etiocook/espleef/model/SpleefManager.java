85c5616ab65e402fc8f084a7657aad698e58aa86
CassyusKley <eTioCooK99@gmail.com>
2020-09-13 03:27:17 -0300

    updated version 1.0

--- src/main/java/etiocook/espleef/model/SpleefManager.java
+++ src/main/java/etiocook/espleef/model/SpleefManager.java
@@ -1,10 +1,14 @@
 package etiocook.espleef.model;
 
 import etiocook.espleef.enums.SpleefState;
+import etiocook.espleef.utils.Messages;
 import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SpleefManager {
 
@@ -27,6 +31,29 @@ public class SpleefManager {
 
     public void remove(String name){
         getSpleefList().remove(name);
+    }
+
+    public void announcement(){
+        AtomicInteger announceCount = new AtomicInteger();
+
+        if (announceCount.getAndIncrement() <= 3) {
+
+            return;
+        }
+
+        if (getSpleefList().size() <= 1) {
+            getSpleefList().forEach(names -> {
+                Player playersList = Bukkit.getPlayer(names);
+
+                remove(playersList.getName());
+                playersList.getInventory().clear();
+                playersList.performCommand("spawn");
+            });
+
+            Bukkit.broadcastMessage(Messages.LACK_PLAYERS);
+            return;
+        }
+
     }
 
 }
--- src/main/java/etiocook/espleef/utils/Messages.java
+++ src/main/java/etiocook/espleef/utils/Messages.java
@@ -4,5 +4,6 @@ public class Messages {
 
     public static String NOT_STARTED = "§cThe Event has not yet started";
     public static String PVP_DISABLED = "§cPvp is disabled in the event";
+    public static String LACK_PLAYERS=  "§cEvent was canceled due to lack of players";
 
 }
